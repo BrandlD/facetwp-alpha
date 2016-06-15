193aad85fa42f069dcab06758f54462d77ab9ccb
mgibbs189 <mgibbs189@gmail.com>
2016-06-15 17:09:31 -0400

    Tagged 1.1

--- index.php
+++ index.php
@@ -3,30 +3,14 @@
 Plugin Name: FacetWP - Alpha
 Plugin URI: https://facetwp.com/
 Description: Alphabetical letter facet
-Version: 1.0.4
+Version: 1.1
 Author: FacetWP, LLC
-Author URI: https://facetwp.com/
-GitHub Plugin URI: https://github.com/FacetWP/facetwp-alpha
-
-Copyright 2016 FacetWP, LLC
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, see <http://www.gnu.org/licenses/>.
+GitHub URI: facetwp/facetwp-alpha
 */
 
-// exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+defined( 'ABSPATH' ) or exit;
 
+include( dirname( __FILE__ ) . '/github-updater.php' );
 
 /**
  * WordPress init hook
